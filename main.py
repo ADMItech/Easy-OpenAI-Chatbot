bce1d07f34ae0fbe88a6a1f74039246abf056c54
MichalDz <info@admi.tech>
2023-10-11 09:33:03 +0200

    Working English version

--- main.py
+++ main.py
@@ -8,12 +8,23 @@
 import openai
 
 # Connection Key to openAI
-openai.api_key = " "  # add your own key - go to web and generate one
+openai.api_key = "mykey"  # add your own key - go to web and generate one
 
 
 # FUNCTIONS
-def chat_with_bot(question):
+def chat_with_bot(question1):
+    """Function takes question and returns answer of ChatGPT"""
     answer = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = [{"role": "user", "content": question}]
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": question1}]
     )
+    return answer.choices[0].message.content.strip()
+
+
+# MAIN LOOP
+if __name__ == "__main__":
+    while True:
+        question = input("You :")
+        if question.lower() in ["quit", "exit"]:
+            break
+        print("ChatBot :", chat_with_bot(question))
